82f742e67f92d717d09cdba4aef9bf44a7e0b382
jose.antonio.martin.crespo <jose.antonio.martin.crespo@devoteam.com>
2021-03-06 16:01:38 +0100

    JWT middleware

---
+++ middlewares/validation-jwt.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const validateJWT = (req, res, next) => {
+
+    //Leer el token
+
+    const token = req.header('x-token');
+
+
+    if(!token){
+        return res.status(401).json({
+            ok: false,
+            msg: 'No token sent'
+        });
+    }
+
+    try {
+
+        const {uid} = jwt.verify(token, process.env.JWT_KEY);
+        
+        req.uid = uid;
+
+        next();
+        
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No valid token sent'
+        });
+    }
+
+
+}
+
+
+module.exports = {
+    validateJWT
+}
--- routes/users.js
+++ routes/users.js
@@ -6,18 +6,21 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 const {validateFields} = require('../middlewares/validation-fields')
-const {getUsers, createUser, updateUser, deleteUser} = require('../controllers/users')
+const {getUsers, createUser, updateUser, deleteUser} = require('../controllers/users');
+const { validateJWT } = require('../middlewares/validation-jwt');
 
 const router = Router();
 
 router
-.get( '/' , getUsers)
+.get( '/' , validateJWT, getUsers)
 .post( '/' ,
 [
+    validateJWT,
     check('username', 'Username is required').not().isEmpty(),
     check('password', 'Password is required').not().isEmpty(),
     check('email', 'Email is required').isEmail(),
     validateFields
+    
 ], 
 createUser)
 .put( '/:id' , 
@@ -27,7 +30,7 @@ createUser)
     validateFields
 ], 
 updateUser)
-.delete('/:id', deleteUser);
+.delete('/:id', validateJWT, deleteUser);
 
 
 
