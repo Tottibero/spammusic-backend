54f1ef20391a51cf7b71f75b45fd7c50ad2f8d75
jose.antonio.martin.crespo <jose.antonio.martin.crespo@devoteam.com>
2021-03-06 15:11:01 +0100

    minor fix

--- .vscode/helpers/jwt.js
+++ .vscode/helpers/jwt.js
@@ -8,7 +8,7 @@ const generateJWT = (uid) => {
             uid
         };
     
-        jwt.sign( payload, process.env.JWT_SECRET, {
+        jwt.sign( payload, process.env.JWT_KEY, {
             expiresIn: '12h'
         }, (err, token) =>{
     
